e3543720c4c58cd067ba0a0b4db7683ce7afbcb2
harshal-0101 <mahajanharshalnitin@gmail.com>
2024-08-01 07:16:19 +0530

    Portfolio website on going project

---
+++ portfolio.js
@@ -0,0 +1,38 @@
+var wordsToType = document.querySelector("span[words]").getAttribute("words").split(','),
+    typer = document.querySelector("span[words]"),
+    typingSpeed = (parseInt(typer.getAttribute('typing-speed')) || 70),
+    typingDelay = (parseInt(typer.getAttribute('typing-delay')) || 700);
+
+var currentWordIndex = 0,
+    currentCharacterIndex = 0;
+
+function type() {
+
+    var wordToType = wordsToType[currentWordIndex % wordsToType.length];
+
+    if (currentCharacterIndex < wordToType.length) {
+        typer.innerHTML += wordToType[currentCharacterIndex++];
+        setTimeout(type, typingSpeed);
+    } else {
+
+        setTimeout(erase, typingDelay);
+    }
+
+}
+
+function erase() {
+    var wordToType = wordsToType[currentWordIndex % wordsToType.length];
+    if (currentCharacterIndex > 0) {
+        typer.innerHTML = wordToType.substr(0, --currentCharacterIndex - 1);
+        setTimeout(erase, typingSpeed);
+    } else {
+
+        currentWordIndex++;
+        setTimeout(type, typingDelay);
+    }
+
+}
+
+// window.onload = function() {
+//     document.getElementById('loader').style.display = 'black';
+// };
